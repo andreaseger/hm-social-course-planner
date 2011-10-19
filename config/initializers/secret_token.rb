2e1fcfd7663c2020bab3b9e6464d89b07edc9588
Eger Andreas <github@eger-andreas.de>
2011-10-19 18:10:12 +0200

    create initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialCoursePlanner::Application.config.secret_token = 'f969c109f10c575a3d6e90f2634dff91b0e07f24a2d65c9ac2841b0886758e9663a45dfaf37004e2403d840fc522d1a6807abdf6781201351fbf9086b79e2a2b'
