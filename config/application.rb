589649e3456252aee217a44069fe13bb2343d997
Andreas Eger <dev@eger-andreas.de>
2011-12-13 15:00:03 +0100

    forcing ssl connection in production 2 [ci skip]

--- config/application.rb
+++ config/application.rb
@@ -53,8 +53,6 @@ module SocialCoursePlanner
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.force_ssl = false
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
