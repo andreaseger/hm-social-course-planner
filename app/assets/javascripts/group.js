97b0d5394b6be4a520a7d4577c13ed8c6f5abc18
Moritz Kammerer <kammerer@kuponjo.de>
2012-01-03 14:45:21 +0100

    Some code refactoring.

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -3,11 +3,6 @@ function renderScheduleData(data, interactive) {
     
     createTimeslotDivs();
     createDayDivs();
-    $('#day1').append('<h2>Montag</h2>');
-    $('#day2').append('<h2>Dienstag</h2>');
-    $('#day3').append('<h2>Mittwoch</h2>');
-    $('#day4').append('<h2>Donnerstag</h2>');
-    $('#day5').append('<h2>Freitag</h2>');   
 
     for(var b = 0; b < data.bookings.length; b++) {
         var booking = data.bookings[b];
@@ -110,10 +105,13 @@ function calculateLength(start, end) {
 }
 
 function createDayDivs() {
+    var days = [ 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag' ];
+    
     for(var i = 1; i <= 5; i++) {    
         var dayDiv = $(document.createElement('div'));
         dayDiv.attr('id', 'day' + i);
-        $('#content').append(dayDiv);    
+        dayDiv.append('<h2>' + days[i - 1] + '</h2>');
+        $('#content').append(dayDiv);
     }
 }
 
